7d42391b44e072874acb5d484bac9ecbe32b4aad
Juan Carlos Echevarria <juancarlosenrique103@gmail.com>
2022-11-25 19:27:25 -0500

    [feat]: Show greatings in multi languages with js :zap:.

---
+++ index.js
@@ -0,0 +1,47 @@
+const greetings = [
+	"Hello everyone!",
+	"¡Hola a todos!",
+	"Bonjour tout le monde!",
+	"Hallo alle zusammen!",
+	"Buongiorno a tutti!",
+	"Olá a todos!",
+];
+
+const container = document.getElementById("container");
+
+function sleep(milliseconds) {
+	const date = Date.now();
+	let currentDate = null;
+	do {
+		currentDate = Date.now();
+	} while (currentDate - date < milliseconds);
+}
+
+function addElementText(text, className, parent) {
+	const element = document.createElement("h1");
+	element.id = "typed";
+	element.innerText = text;
+	element.className = className;
+	parent.appendChild(element);
+}
+
+function removeElementText(id, parent) {
+	const element = document.getElementById(id);
+	parent.removeChild(element);
+}
+
+const init = (parent, arrayElements, className) => {
+	addElementText(arrayElements[0], className, parent);
+	let index = 1;
+	setInterval(() => {
+		addElementText(arrayElements[index], className, parent);
+		sleep(1);
+		removeElementText(className, parent);
+		index++;
+		if (index === arrayElements.length) {
+			index = 0;
+		}
+	}, 3500);
+};
+
+init(container, greetings, "typed");
